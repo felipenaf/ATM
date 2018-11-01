9d48d517257a5ab5319d58a9274a6338af392a60
Felipe <felipe.ferreira0192@gmail.com>
2018-11-01 18:00:59 -0300

    query atualizada, tipo foi para tabela documento

--- query.sql
+++ query.sql
@@ -1,29 +1,48 @@
 CREATE DATABASE atm CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE atm.pessoa(
+CREATE TABLE atm.pessoa (
     id int(5) AUTO_INCREMENT,
     login VARCHAR(50) NOT NULL,
     senha varchar(50) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     UNIQUE (login),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE atm.pessoaFisica(
-    cpf varchar(14) NOT NULL,
-    nome VARCHAR(50) NOT NULL,
+CREATE TABLE atm.documento (
+    id int(5) AUTO_INCREMENT,
+    tipo VARCHAR(2) NOT NULL,
+    numero varchar(18) NOT NULL,
     idPessoa int(5) NOT NULL,
-    PRIMARY KEY (cpf),
+    PRIMARY KEY (id),
     FOREIGN KEY (idPessoa) REFERENCES atm.pessoa(id)
-)
+);
 
-CREATE TABLE atm.pessoaJuridica(
-    cnpj varchar(18) NOT NULL,
-    razaoSocial VARCHAR(50) NOT NULL,
-    idPessoa int(5) NOT NULL,
-    PRIMARY KEY (cnpj),
-    FOREIGN KEY (idPessoa) REFERENCES atm.pessoa(id)
-)
+INSERT INTO atm.pessoa (login, senha, nome) VALUES ('admin', 'admin', 'Administrador');
 
+-- CREATE TABLE atm.pessoaFisica(
+--     cpf varchar(14) NOT NULL,
+--     nome VARCHAR(50) NOT NULL,
+--     idPessoa int(5) NOT NULL,
+--     PRIMARY KEY (cpf),
+--     FOREIGN KEY (idPessoa) REFERENCES atm.pessoa(id)
+-- )
+
+-- CREATE TABLE atm.pessoaJuridica(
+--     cnpj varchar(18) NOT NULL,
+--     razaoSocial VARCHAR(50) NOT NULL,
+--     idPessoa int(5) NOT NULL,
+--     PRIMARY KEY (cnpj),
+--     FOREIGN KEY (idPessoa) REFERENCES atm.pessoa(id)
+-- )
+
+-- select * from atm.pessoaFisica 
+-- inner join 
+--     atm.pessoa 
+-- on
+--     atm.pessoaFisica.idPessoa = atm.pessoa.id
+-- where
+--     cpf = '402' and senha = '123';
 
 -- CREATE TABLE pessoa(
 --     id int(5) AUTO_INCREMENT PRIMARY KEY,
@@ -31,7 +50,6 @@ CREATE TABLE atm.pessoaJuridica(
 --     idade int(3),
 --     senha varchar(50) NOT NULL
 -- )
-
 
 -- CREATE TABLE atm.pessoaJuridica(
 --     cnpj varchar(18) NOT NULL,
@@ -46,4 +64,3 @@ CREATE TABLE atm.pessoaJuridica(
 --     PRIMARY KEY (cpf),
 --     FOREIGN KEY (pessoaId) REFERENCES pessoa(id)
 -- )
-
