2d142e3eb1402264e90a78d5228fc3da3e448f86
Felipe <felipe.ferreira0192@gmail.com>
2018-11-04 23:09:32 -0200

    gerador de número de conta corrente

---
+++ gerador.py
@@ -0,0 +1,10 @@
+import random
+from time import time as t
+
+def xrange(x):
+
+    return iter(range(x))
+
+def nrConta():                                                                                             
+    n = [random.randrange(10) for i in xrange(9)]                                                                                              
+    return "%d%d.%d%d%d%d%d%d.%d" % tuple(n)
