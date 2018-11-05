5c680f95e170eb3f71fdff1955230652dc10d4ea
Felipe <felipe.ferreira0192@gmail.com>
2018-11-04 23:12:26 -0200

    bug fix, inclusão de exception, implementação do getbyloginsenha

--- pessoaDao.py
+++ pessoaDao.py
@@ -1,4 +1,5 @@
 from pessoa import Pessoa
+from contaCorrente import ContaCorrente
 from connection import Connection
 import time
 
@@ -8,40 +9,49 @@ class PessoaDao():
         try:
             con = Connection.instance()
             cursor = con.cursor()
-            cursor.execute("SELECT * from atm.pessoa WHERE login = %s and senha = %s;", (login, senha))
-        except:
-            print('\nAlgo deu errado.')
+            cursor.execute("SELECT * from atm.pessoa "
+                            "LEFT JOIN atm.documento "
+                                "ON atm.pessoa.id = atm.documento.idPessoa "
+                            "LEFT JOIN atm.contaCorrente "
+                                "ON atm.pessoa.id = atm.contaCorrente.idPessoa "
+                            "WHERE login = %s and senha = %s;", (login, senha))
+        except Exception as e:
+            print('\nAlgo deu errado.\n', e)
         else:
             return cursor.fetchone()
         finally:
             con.close()
-            
 
-    def cadastrar(self, pessoa):
+    def cadastrar(self, pessoa, conta):
         try:
             con = Connection.instance()
             cursor = con.cursor()
             dados = (pessoa.getLogin(), pessoa.getSenha(), pessoa.getNome())
             cursor.execute("INSERT INTO pessoa (login, senha, nome) VALUES (%s, %s, %s);", dados)
-            con.commit()
-            dadosDoc = (pessoa.getTipo(), pessoa.getDocumento(), cursor.lastrowid)
+            ultimoId = cursor.lastrowid
+            dadosDoc = (pessoa.getTipo(), pessoa.getDocumento(), ultimoId)
             cursor.execute("INSERT INTO documento (tipo, numero, idPessoa) VALUES (%s, %s, %s);", dadosDoc)
+            dadosConta = (conta.getNumeroCC(), conta.getNumeroAgencia(), conta.getSaldo(), ultimoId)
+            cursor.execute("INSERT INTO contaCorrente (numero, agencia, saldo, idPessoa) VALUES (%s, %s, %s, %s);", dadosConta)
             con.commit()
-        except:
-            print('\nNão foi possível realizar o cadastro!\n')
+        except Exception as e:
+            print('\nNão foi possível realizar o cadastro!\n', e)
         else:
-            print('\nCliente cadastrado com sucesso!\n')
+            print('\nCliente cadastrado com sucesso!')
+            print('\nNúmero da Agência:', conta.getNumeroAgencia())
+            print('\nNúmero da Conta:', conta.getNumeroCC())
         finally:
             time.sleep(3)
             con.close()
+            input('\nPressione <enter> para concluir')
 
     def listar(self):
         try:
             con = Connection.instance()
             cursor = con.cursor()
-            cursor.execute("SELECT * from atm.pessoa INNER JOIN atm.documento on atm.documento.idPessoa = atm.pessoa.id;")
-        except:
-            print('\nAlgo deu errado.')
+            cursor.execute("SELECT * from atm.pessoa INNER JOIN atm.documento on atm.documento.idPessoa = atm.pessoa.id INNER JOIN atm.contaCorrente on atm.contaCorrente.idPessoa = atm.pessoa.id;")
+        except Exception as e:
+            print('\nAlgo deu errado.\n', e)
         else:
             result = cursor.fetchall()
             return result
@@ -53,13 +63,17 @@ class PessoaDao():
             con = Connection.instance()
             cursor = con.cursor()
             cursor.execute("DELETE FROM atm.documento WHERE idPessoa = %s;", (pessoa.getId(),))
-            con.commit()
+            cursor.execute("DELETE FROM atm.contaCorrente WHERE idPessoa = %s;", (pessoa.getId(),))
             cursor.execute("DELETE FROM atm.pessoa WHERE id = %s;", (pessoa.getId(),))
             con.commit()
-        except:
-            print('\nNão foi possível realizar a exclusão!\n')
+        except Exception as e:
+            print('\nNão foi possível realizar a exclusão!\n', e)
         else:
             print('\nCliente excluído com sucesso!\n')
         finally:
             time.sleep(3)
             con.close()
+            input('\nPressione <enter> para concluir')
+
+    def editar(self):
+        pass
